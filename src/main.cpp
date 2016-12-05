e541e8e32b2acfe56a706ac798e5f96c290f65ff
Lukas Jelinek <lukx19@gmail.com>
2016-12-05 19:13:27 +0100

    Firs working line follower

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,103 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-#define button_pin 2
-#define infra_low 3
-#define infra_high 8
+#define button_pin 9
+#define first_infra 3
 
 Servo left;
 Servo right;
+
+bool ir_sensors[5];
+bool start = false;
+
+bool buttonDown() { return !digitalRead(button_pin); }
+
+void forward(int speed = 200) {
+  left.write(1500 + speed);
+  right.write(1500 - speed);
+}
+
+void backward(int speed = 200) {
+  left.write(1500 - speed);
+  right.write(1500 + speed);
+}
+
+void stop() {
+  left.write(1500);
+  right.write(1500);
+}
+
+/* inplace turning */
+void turn90(int slope) {
+  left.write(1500 + slope);
+  right.write(1500 + slope);
+}
+
+/* turning on smoother curve */
+void turnCurve(int slope, int speed = 200) {
+  left.write(1500 + speed + slope);
+  right.write(1500 - speed + slope);
+}
+
+void debugInfra() {
+  for (int i = 0; i < sizeof(ir_sensors); ++i) {
+    Serial.print(!ir_sensors[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+}
+
+void readInfra() {
+  for (int i = 0; i < sizeof(ir_sensors); ++i) {
+    ir_sensors[i] = !digitalRead(first_infra + i);
+  }
+}
+
+// /* indicates if any of the sensors is on the line */
+// bool lineAhead() {
+//   bool res = 0;
+//   for (int i = 0; i < sizeof(ir_sensors); ++i) {
+//     res |= !ir_sensors[i];
+//   }
+//   return res;
+// }
+
+void setup() {
+  // initialize serial and wait for port to open:
+  Serial.begin(115200);
+  left.attach(12);
+  right.attach(13);
+  // button
+  pinMode(button_pin, INPUT_PULLUP);
+
+  stop();
+  // setup infa array
+  for (int i = first_infra; i < first_infra + sizeof(ir_sensors); ++i) {
+    pinMode(i, INPUT);
+  }
+}
+
+void loop() {
+  readInfra();
+  Serial.println(buttonDown());
+  debugInfra();
+
+  if (buttonDown()) {
+    start = true;
+  }
+  if (!start) {
+    stop();
+    return;
+  }
+
+  if (ir_sensors[2]) {
+    forward(200);
+  } else if (ir_sensors[1]) {
+    turnCurve(-70);
+  } else if (ir_sensors[3]) {
+    turnCurve(70);
+  } else { // finding line
+    turn90(-30);
+  }
+  delay(1);
+}
